c6410c164c6c516ccdcd181cbbdf4f9b6d25ce40
Corinn Herrick <herrickc@mit.edu>
2015-05-25 18:52:05 -0400

    Changed handler for retreiving well data

--- InitiativeWASH/gateway/handlers.py
+++ InitiativeWASH/gateway/handlers.py
@@ -52,16 +52,33 @@ class NeighborhoodHandler(KeywordHandler):
     keyword = "near"
 
     def help(self):
-        self.respond("Text in 'near NEIGHBORHOOD' to get the water sources near you.")
+        self.respond("Text in 'near NEIGHBORHOOD' to get the water sources near you. Text in 'near NEIGHBORHOOD, WELLID to get data about a specific water source.")
         return True
 
     def handle(self, text):
-        neighborhood = Neighborhood.objects.get(name__iexact=text.strip())
+        response = "Thank you for using WASH Mobile.\n"
+        data_list = text.split(",")
+        if len(data_list) == 0:
+            self.help()
+            return True
+        elif len(data_list) == 2:
+            try:
+                well_id = int(data_list[1])
+                try:
+                    source = Source.objects.get(id=well_id)
+                    response += "Here is the grade for the well you entered.\n\n"
+                    response += "Well %d, Grade: %d\n\n" % (source.id, source.water_quality())
+                except Source.DoesNotExist:
+                    response += "We can't find a water source with ID %s\n" % (data_list[1],)
+            except ValueError:
+                response += "We can't find a water source with ID %s\n" % (data_list[1],)
+            
+        neighborhood = Neighborhood.objects.get(name__iexact=data_list[0].strip())
         sources_nearby = sorted(Source.objects.all(), key=lambda s: s.water_quality(), reverse=True)
         sources_nearby = filter(lambda s: neighborhood.distance_to(s.lattitude, s.longitude) < neighborhood.radius_to_search,
                                 sources_nearby)
         
-        response = "Here is a list of wells near you:\n"
+        response += "Here are the best water sources near you:\n"
         response += "\n".join(["Well %d, Grade: %d\n" % (source.id, source.water_quality()) for source in sources_nearby[:3]])
         response += "\nThank you for using I:WASH"
         response += str(len(sources_nearby))
