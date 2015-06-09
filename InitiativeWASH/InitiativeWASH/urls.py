21ff45b62bb4b591ad2afb9a939086bc0250f4ee
wcordelo <will96@mit.edu>
2015-06-09 14:39:16 -0400

    made minor change
    minor change

--- InitiativeWASH/InitiativeWASH/urls.py
+++ InitiativeWASH/InitiativeWASH/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from rtwilio.views import TwilioBackendView
+# from rtwilio.views import TwilioBackendView
 from rapidsms_telerivet.views import TelerivetBackendView
 
 admin.autodiscover()
